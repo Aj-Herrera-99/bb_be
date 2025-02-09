769a4e3243bb17eefd7380a84282f4ff0736bbed
Simone-Fratini <simone@fratini.com>
2025-02-09 13:11:56 +0100

    Add SSL configuration for MySQL database connection

--- data/db.js
+++ data/db.js
@@ -1,10 +1,18 @@
 const mysql = require("mysql2");
+const fs = require("fs");
+const path = require("path");
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, '../ssl/ca.pem'))
+    }
 });
+
 connection.connect((err) => {
     if (err) throw err;
     console.log("Connected to MySQL!");
